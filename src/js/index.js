659397a33ea09262cee6ec4d4935cdc5944244d2
Estevo Vinicius <viniestevo33@gmail.com>
2023-03-12 12:13:03 -0300

    work on flow carrosel, img and btn, remove or add

--- src/js/index.js
+++ src/js/index.js
@@ -1,27 +1,39 @@
-const btn_carrosel = document.querySelectorAll('.btn');
+const btnCarrosel = document.querySelectorAll('.btn');
 
-const btn1 = document.addEventListener('click', () => {
+// const btn1 = document.addEventListener('click', () => {
 
-})
+// })
 
-const btn2 = document.addEventListener('click', () => {
+// const btn2 = document.addEventListener('click', () => {
 
+// });
+
+// const btn3 = document.addEventListener('click', () => {
+
+// });
+
+// const btn4 = document.addEventListener('click', () => {
+
+// });
+
+// const btn5 = document.addEventListener('click', () => {
+
+// });
+
+// const btn6 = documnt.addEvenListener('click', () => {
+
+// });
+
+btnCarrosel.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        const btnSelect = document.querySelector('.select');
+        btnSelect.classList.remove('select');
+
+        btn.classList.add('select');
+
+        const imgAtive = document.querySelector('.ative');
+        imgAtive.classList.remove('ative');
+
+    })
 });
-
-const btn3 = document.addEventListener('click', () => {
-
-});
-
-const btn4 = document.addEventListener('click', () => {
-
-});
-
-const btn5 = document.addEventListener('click', () => {
-
-});
-
-const btn6 = documnt.addEvenListener('click', () => {
-
-});
-
 
