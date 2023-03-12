beacc63e010fb49c28c751d81f6e88f219eec270
Estevo Vinicius <viniestevo33@gmail.com>
2023-03-12 11:47:38 -0300

    add const btn

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,27 @@
 const btn_carrosel = document.querySelectorAll('.btn');
 
+const btn1 = document.addEventListener('click', () => {
+
+})
+
+const btn2 = document.addEventListener('click', () => {
+
+});
+
+const btn3 = document.addEventListener('click', () => {
+
+});
+
+const btn4 = document.addEventListener('click', () => {
+
+});
+
+const btn5 = document.addEventListener('click', () => {
+
+});
+
+const btn6 = documnt.addEvenListener('click', () => {
+
+});
+
+
